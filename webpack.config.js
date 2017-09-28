a3b9c8c78e00abf8e3de711e96ec799fc9ce9602
Jesse Stuart <jdstuart@icloud.com>
2017-09-28 02:30:36 -0400

    Revert "[build] Include un-uglified build in `dist`."
    This reverts commit 2353658bfb121e7ca2291564931c2ce8bc1b3946.

--- webpack.config.js
+++ webpack.config.js
@@ -8,23 +8,22 @@ const CleanPlugin = require('clean-webpack-plugin');
 const REPO_ROOT = __dirname;
 const ASSETS_PATH = path.resolve(REPO_ROOT, 'dist');
 
-module.exports = env => ({
+module.exports = (env) => ({
   entry: './src/index.js',
   output: {
     path: ASSETS_PATH,
-    filename:
-      env === 'dist:min' ? 'tt-react-calendar.min.js' : 'tt-react-calendar.js',
+    filename: 'index.js',
     library: 'tt-react-calendar',
     libraryTarget: 'umd',
   },
   externals: {
-    react: {
+    'react': {
       commonjs: 'react',
       commonjs2: 'react',
       amd: 'react',
       root: 'React',
     },
-    moment: {
+    'moment': {
       commonjs: 'moment',
       commonjs2: 'moment',
       amd: 'moment',
@@ -32,12 +31,12 @@ module.exports = env => ({
     },
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+    ],
   },
   plugins: compact([
-    process.env.CLEAN_BUILD
-      ? new CleanPlugin([ASSETS_PATH], { root: REPO_ROOT })
-      : null,
-    env === 'dist:min' ? new webpack.optimize.UglifyJsPlugin() : null,
+    new CleanPlugin([ASSETS_PATH], { root: REPO_ROOT }),
+    (env === 'dist:min' ? new webpack.optimize.UglifyJsPlugin() : null),
   ]),
-});
+})
